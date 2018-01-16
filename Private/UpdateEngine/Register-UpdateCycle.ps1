8b643e02c53dd45e74e346f492cddf0b0ccfa5da
Lars Slouborg Bengtsson <LABE@danskespil.dk>
2018-01-16 15:55:17 +0100

    dot source trick inside Register-UpdateCycle as well.

--- Private/UpdateEngine/Register-UpdateCycle.ps1
+++ Private/UpdateEngine/Register-UpdateCycle.ps1
@@ -32,74 +32,88 @@ function Register-UpdateCycle() {
     #############
     # Execution #
     #############
-    <#
+    Begin {
+        <#
+            - Load modules
+                > dot sourcing as we do a remove-module | import-module trick in order to get the newest version of HealOps before executing Register-UpdateCycle.
+                This is done inside the Invoke-HealOps module.
+        #>
+        . $PSScriptRoot/Get-LatestModuleVersionLocally.ps1
+        . $PSScriptRoot/../ConfigurationManagement/Get-HealOpsConfig.ps1
+        . $PSScriptRoot/../ConfigurationManagement/Compare-HealOpsConfig.ps1
+        . $PSScriptRoot/../ConfigurationManagement/Sync-HealOpsConfig.ps1
+    }
+    Process {
+        <#
         - An update cycle ran. Register it.
-    #>
-    # Determine DateTime for checkForUpdatesNext. Using a random plus "M" minutes, in order to NOT overload the Package Management backend with requests at the same time from multiple instances of HealOps. In this way update requests will be more evenly spread out.
-    $checkForUpdatesNext_DateTimeRandom = get-random -Minimum 1 -Maximum 123
-    if ($Config.checkForUpdatesInterval_Hours.length -ge 1) {
-        # Use the interval from the HealOps config json file.
-        $checkForUpdatesNext = (get-date).AddHours($Config.checkForUpdatesInterval_Hours).AddMinutes(($checkForUpdatesNext_DateTimeRandom)).ToString()
-    } else {
-        # Fall back to a default interval of 1 day. As the checkForUpdatesInterval_Hours property in the config file was corrupt.
-        $checkForUpdatesNext = (get-date).AddHours(1).AddMinutes(($checkForUpdatesNext_DateTimeRandom)).ToString()
-    }
-
-    <#
-        - Compare HealOps configs. The current and the one from the update.
-            > In order to catch changes and reflect them to the HealOps config file that will be used going forward.
-    #>
-    # Get the config from the update
-    [PSModuleInfo]$MainModule = Get-LatestModuleVersionLocally -ModuleName "HealOps"
-    [System.Array]$updatedHealOpsConfig = Get-HealOpsConfig -ModuleBase $MainModule.ModuleBase
-    [Array]$configComparisonResult = Compare-HealOpsConfig -currentConfig $Config -updatedConfig $updatedHealOpsConfig
-
-    # Determine the config file to use as the one going forward
-    if ($configComparisonResult.Count -ge 1) {
-        # Differences was found. Reflect them.
-        try {
-            [PSCustombObject]$syncedConfig = Sync-HealOpsConfig -configChanges $configComparisonResult -currentConfig $Config
-            $syncFailed = $false
-        } catch {
-            $log4netLogger.error("$_")
-            $syncFailed = $true
+        #>
+        # Determine DateTime for checkForUpdatesNext. Using a random plus "M" minutes, in order to NOT overload the Package Management backend with requests at the same time from multiple instances of HealOps. In this way update requests will be more evenly spread out.
+        $checkForUpdatesNext_DateTimeRandom = get-random -Minimum 1 -Maximum 123
+        if ($Config.checkForUpdatesInterval_Hours.length -ge 1) {
+            # Use the interval from the HealOps config json file.
+            $checkForUpdatesNext = (get-date).AddHours($Config.checkForUpdatesInterval_Hours).AddMinutes(($checkForUpdatesNext_DateTimeRandom)).ToString()
+        } else {
+            # Fall back to a default interval of 1 day. As the checkForUpdatesInterval_Hours property in the config file was corrupt.
+            $checkForUpdatesNext = (get-date).AddHours(1).AddMinutes(($checkForUpdatesNext_DateTimeRandom)).ToString()
         }
-    } else {
-        # No changes found. Debug log.
-        $log4netLoggerDebug.Debug("There was no changes found when comparing the current HealOps config and the updated one. All good in the hood.`
-        Count on the compare $($configComparisonResult.Count)")
+
+        <#
+        - Compare HealOps configs. The current and the one from the update.
+        > In order to catch changes and reflect them to the HealOps config file that will be used going forward.
+        #>
+        # Get the config from the update
+        [PSModuleInfo]$MainModule = Get-LatestModuleVersionLocally -ModuleName "HealOps"
+        [System.Array]$updatedHealOpsConfig = Get-HealOpsConfig -ModuleBase $MainModule.ModuleBase
+        [Array]$configComparisonResult = Compare-HealOpsConfig -currentConfig $Config -updatedConfig $updatedHealOpsConfig
+
+        # Determine the config file to use as the one going forward
+        if ($configComparisonResult.Count -ge 1) {
+            # Differences was found. Reflect them.
+            try {
+                [PSCustombObject]$syncedConfig = Sync-HealOpsConfig -configChanges $configComparisonResult -currentConfig $Config
+                $syncFailed = $false
+            } catch {
+                $log4netLogger.error("$_")
+                $syncFailed = $true
+            }
+        } else {
+            # No changes found. Debug log.
+            $log4netLoggerDebug.Debug("There was no changes found when comparing the current HealOps config and the updated one. All good in the hood.`
+            Count on the compare $($configComparisonResult.Count)")
+        }
+
+        if (-not $syncFailed) {
+            # The sync succeeded. Use the synced config object.
+            [PSCustombObject]$configToUse = $syncedConfig
+        } else {
+            # The sync failed. Use the non-updated current config object. Falling back to this as the chance of this working is higher that a non-properly updated config object. At least that is the assumption
+            [PSCustombObject]$configToUse = $Config
+        }
+
+        # When in verbose mode & for reference
+        Write-Verbose -Message "The value of checkForUpdatesNext > $checkForUpdatesNext"
+        $log4netLoggerDebug.debug("Register-UpdateCycle > The value of checkForUpdatesNext > $checkForUpdatesNext")
+
+        # Update the HealOps config json file
+        $configToUse.checkForUpdatesNext = $checkForUpdatesNext
+
+        # Convert the JSON
+        $ConfigInJSON = ConvertTo-Json -InputObject $configToUse -Depth 3
+
+        # Update the HealOps config json file
+        try {
+            # Record the update cycle data to the HealOps modules config file.
+            Set-Content -Path "$ModuleBase/Artefacts/HealOpsConfig.json" -Value $ConfigInJSON -Force -Encoding UTF8 -ErrorAction Stop
+
+            # Return
+            $true
+        } catch {
+            # Log it
+            $log4netLogger.error("Register-UpdateCycle > Failed to write the config json file for the module > $ModuleBase. Failed with > $_")
+
+            # Return
+            $false
+        }
     }
-
-    if (-not $syncFailed) {
-        # The sync succeeded. Use the synced config object.
-        [PSCustombObject]$configToUse = $syncedConfig
-    } else {
-        # The sync failed. Use the non-updated current config object. Falling back to this as the chance of this working is higher that a non-properly updated config object. At least that is the assumption
-        [PSCustombObject]$configToUse = $Config
-    }
-
-    # When in verbose mode & for reference
-    Write-Verbose -Message "The value of checkForUpdatesNext > $checkForUpdatesNext"
-    $log4netLoggerDebug.debug("Register-UpdateCycle > The value of checkForUpdatesNext > $checkForUpdatesNext")
-
-    # Update the HealOps config json file
-    $configToUse.checkForUpdatesNext = $checkForUpdatesNext
-
-    # Convert the JSON
-    $ConfigInJSON = ConvertTo-Json -InputObject $configToUse -Depth 3
-
-    # Update the HealOps config json file
-    try {
-        # Record the update cycle data to the HealOps modules config file.
-        Set-Content -Path "$ModuleBase/Artefacts/HealOpsConfig.json" -Value $ConfigInJSON -Force -Encoding UTF8 -ErrorAction Stop
-
-        # Return
-        $true
-    } catch {
-        # Log it
-        $log4netLogger.error("Register-UpdateCycle > Failed to write the config json file for the module > $ModuleBase. Failed with > $_")
-
-        # Return
-        $false
-    }
+    End {}
 }
